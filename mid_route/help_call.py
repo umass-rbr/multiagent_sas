766ae2e16906c9c7c2968f470a52407a8714900e
Jason Zhuo <jzhuo@umass.edu>
2018-12-12 10:23:09 -0500

    modified help_call to be naive

--- mid_route/help_call.py
+++ mid_route/help_call.py
@@ -10,6 +10,9 @@ def help(attemptedAction):
         'denial'
     ]
     # randomly select response, for now
-    response = random.randint(0,3)
+    #response = random.randint(0,3)
 
-    return responseList[response]
+    #return responseList[response]
+
+    # return the proposed action and just take it
+    return attemptedAction
