afc825dcf7a2c0938e93e483c71966563841e157
Jason Zhuo <jzhuo@umass.edu>
2019-01-04 10:53:40 -0500

    Added new maps and features

--- mid_route/campus_map.py
+++ mid_route/campus_map.py
@@ -1,46 +1,95 @@
-def generate_map():
-    campus_map = dict()
+def generate_map(map_number):
+    
+    # initializing the different campus maps
+    campus_maps = [dict() for index in range(3)]
+
+    # initializing the different obstacles
     
     '''
         keys represent nodes on directed graph to other connected nodes
         values represent list of destination nodes and the cost for travel 
         items in value are -- (destination, cost)
-        cost: 2 downhill, 4 level, 6 uphill, 10 stay 
+        cost: 2 downhill, 4 level, 6 uphill, 10 crosswalk 
     '''
     
-    campus_map[0] = [
-        (1, 6),
-        (2, 4)
+    # creating first map
+    campus_maps[0][0] = [
+        (1, 4, "none")
     ]
-    campus_map[1] = [
-        (3, 4),
-        (0, 2)
+    campus_maps[0][1] = [
+        (0, 4, "none")
+        (2, 10, "crosswalk")
     ]
-    campus_map[2] = [
-        (4, 4), 
-        (6, 2),
-        (0, 4)
+    campus_maps[0][2] = [
+        (1, 10, "crosswalk")
+        (3, 10, "crosswalk")
     ]
-    campus_map[3] = [
-        (4, 2),
-        (1, 4) 
+    campus_maps[0][3] = [
+        (2, 10, "crosswalk")
+        (4, 4, "none")
+        
     ]
-    campus_map[4] = [
-        (3, 6),
-        (7, 4),
-        (5, 4),
-        (2, 4)
-    ]
-    campus_map[5] = [
-        (4, 4), 
-        (6, 2)
-    ]
-    campus_map[6] = [
-        (5, 6), 
-        (2, 6)
-    ]
-    campus_map[7] = [
-        (4, 4) 
+    campus_maps[0][4] = [
+        (3, 4, "none")
     ]
 
-    return campus_map
+
+    # creating second map
+    campus_maps[1][0] = [
+        (1, 6, "none"),
+        (2, 4, "none")
+    ]
+    campus_maps[1][1] = [
+        (3, 4, "none"),
+        (0, 2, "none")
+    ]
+    campus_maps[1][2] = [
+        (4, 4, "none"), 
+        (6, 2, "none"),
+        (0, 4, "none")
+    ]
+    campus_maps[1][3] = [
+        (4, 2, "none"),
+        (1, 4, "none") 
+    ]
+    campus_maps[1][4] = [
+        (3, 6, "none"),
+        (7, 10, "crosswalk"),
+        (5, 4, "none"),
+        (2, 4, "none")
+    ]
+    campus_maps[1][5] = [
+        (4, 4, "none"), 
+        (6, 2, "none")
+    ]
+    campus_maps[1][6] = [
+        (5, 6, "none"), 
+        (2, 6, "none")
+    ]
+    campus_maps[1][7] = [
+        (4, 10, "crosswalk") 
+    ]
+
+
+    # creating third map
+    campus_maps[2][0] = [
+        (1, 10, "crosswalk")
+        (2, 10, "crosswalk")
+    ]
+    campus_maps[2][1] = [
+        (0, 10, "crosswalk")
+        (3, 10, "crosswalk")
+    ]
+    campus_maps[2][2] = [
+        (0, 10, "crosswalk")
+        (3, 10, "crosswalk")
+    ]
+    campus_maps[2][3] = [
+        (2, 10, "crosswalk")
+        (1, 10, "crosswalk")
+        
+    ]
+
+
+    # returning selected map
+    return campus_maps[map_number]
