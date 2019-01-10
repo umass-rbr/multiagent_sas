5382484bd7e423e4eca644bc7be4e4b10323b89f
cbasich <cbasich@cs.umass.edu>
2019-01-10 15:41:58 -0500

    Updated task.py

--- high_rta/task.py
+++ high_rta/task.py
@@ -4,7 +4,17 @@ import itertools as it
 import campus_map
 import robot
 
-class Task():
+class EscortTask():
 	"""docstring for ClassName"""
-	def __init__(self, type, ):
+	def __init__(self, person, pickup, dropoff, start, end):
+		self.person = person
+		self.pickup = pickup
+		self.dropoff = dropoff
+		self.start = start
+		self.end = end
+		
+
+class DeliveryTask(object):
+	"""docstring for DeliveryTask"""
+	def __init__(self, object, start, end):
 		
