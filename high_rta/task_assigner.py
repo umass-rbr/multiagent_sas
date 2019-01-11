4dbf314e33fbd6949638c7e9d3d3e01ebeb96e49
cbasich <cbasich@cs.umass.edu>
2019-01-11 11:29:28 -0500

    Included package.xml for message.

--- high_rta/task_assigner.py
+++ high_rta/task_assigner.py
@@ -1,14 +1,18 @@
 import json
+import rospy
+
+from msg import Problem
 
 import problem_generator
 import robot
-import rospy
 import task
 import utils
 
 WORLDS_DIRECTORY = "worlds/"
 
 def main():
+    pub = rospy.Publisher('Task Assigner', Problem)
+
     with open('mdp_info.json', 'r') as f:
         mdp_info = json.loads(f)
 
@@ -20,8 +24,15 @@ def main():
             for t, r in current_action:
                 # TODO Either need to make it into a task object here, or change high_rta to 
                 # function task objects. The second option is probably better design.
-                problem_file = open((str(r.get_id()) + "_assignment.pddl"), "w+")
-                problem_file.write(problem_generator.generate_escort_problem(r, t, json.load(world_file)))
+                rospy.init_node('Assigner')
+                r = rospy.Rate(10)
+                msg = Problem()
+                msg.problem = problem_generator.generate_escort_problem(r,t,json.load(world_file))
+
+                while not rospy.is_shutdown():
+                    rospy.log_info(msg)
+                    pub.publish(msg)
+                    r.sleep()
 
 
 if __name__ == "__main__":
