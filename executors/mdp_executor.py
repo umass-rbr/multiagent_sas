658d01b0c90270f025939114cbae3e1006457583
Justin Svegliato <justinsvegliato@Justins-MacBook-Pro.local>
2019-01-26 14:09:40 -0500

    Implemented basic structure

--- executors/mdp_executor.py
+++
@@ -1,9 +0,0 @@
-current_location = None
-
-def location_handler(location):
-    current_location = location
-
-def excecute(policy):
-    rospy.Subscriber("monitor/current_location", Location, location_handler)
-    
-    
