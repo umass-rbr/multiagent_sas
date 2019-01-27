5969f72e2a3d8ac2636b50596c981c2a9abfdace
cbasich <cbasich@cs.umass.edu>
2019-01-27 08:49:26 -0500

    Updated task.py to pack info into a JSON str dump for ROS msg purposes.

--- task_assigner/task.py
+++ task_assigner/task.py
@@ -1,12 +1,22 @@
-# TODO Can we make an "abstract" task that has an id, start_time, end_time?
-class DeliveryTask():
-    def __init__(self, id, package, start_location, end_location, start_time, end_time):
+import json
+
+class Task(object):
+    def __init__(self, id, start_time, end_time):
         self.id = id
+        self.start_time = start_time
+        self.end_time = end_time
+
+    def pack(self):
+        return json.dumps(self.__dict__, indent=4)
+
+
+# TODO Can we make an "abstract" task that has an id, start_time, end_time?
+class DeliveryTask(Task):
+    def __init__(self, id, package, start_location, end_location, start_time, end_time):
+        super().__init__(id,start_time,end_time)
         self.package = package
         self.start_location = start_location
         self.end_location = end_location
-        self.start_time = start_time
-        self.end_time = end_time
 
     def get_type(self):
         return 'delivery'
@@ -17,12 +27,10 @@ class DeliveryTask():
 
 class EscortTask():
     def __init__(self, id, person, start_location, end_location, start_time, end_time):
-        self.id = id
+        super().__init__(id,start_time,end_time)
         self.person = person
         self.start_location = start_location
         self.end_location = end_location
-        self.start_time = start_time
-        self.end_time = end_time
 
     def get_type(self):
         return 'escort'
@@ -30,3 +38,6 @@ class EscortTask():
     def __str__(self):
         to_output = "[" + str(start_time) + ", " + str(end_time) + ", " + str(start_location) + ", " + str(end_location) + "]"
         return to_output
+
+t1 = DeliveryTask(0,'package1','2','1','0','3')
+print(t1.pack())
