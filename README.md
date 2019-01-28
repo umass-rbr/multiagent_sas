67b07c0d092995c735cf14d400ca40d2d20f6937
Justin Svegliato <justin.svegliato@gmail.com>
2019-01-28 08:42:45 -0500

    Refactored the code to fit into ROS packages

--- README.md
+++
@@ -1,2 +0,0 @@
-# multiagent_sas
-Multiagent semi-autonomous systems (SAS) for campus delivery robots.
--- task_assigner/item.py
+++
@@ -1,6 +0,0 @@
-class Item():
-    def __init__(self, id, type, position):
-        self.id = id
-        self.type = type
-        self.position = position
-        
