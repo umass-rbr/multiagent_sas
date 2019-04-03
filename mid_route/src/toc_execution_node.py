b99ffc90cd7a852cf13c63b71964dedba42ff9f9
root <root@jzhuoTitan.localdomain>
2019-04-03 00:46:15 -0400

    Added ROS execution node functionality

--- mid_route/src/toc_execution_node.py
+++ mid_route/src/toc_execution_node.py
@@ -10,23 +10,29 @@ from mid_route.src.mdps import RouteMDP
 
 toc_mdp_state_message = None
 
-TOC_ACTION_PUBLISHER = rospy.Publisher("orb_nav/goal", TocMdpAction, queue_size=1)
+TOC_ACTION_PUBLISHER = rospy.Publisher("toc/action", TocMdpAction, queue_size=1)
 
 def toc_mdp_state_callback(message):
     global toc_mdp_state_message
     toc_mdp_state_message = message
 
 def execute(currentState, goalState, mapName):
+	""" Publishes the action to take towards the goal based on current state to the toc/action state """
 
+	# initialize the MDP
     route = RouteMDP(currentState, goalState, mapName)
     route.initialize()
     route.solve()
+
+    # get the action for the current state
     action = route.get_action(currentState)
 
+    # initialize the message to publish
     actionMessage = TocMdpAction()
     actionMessage.location = action[0]
     actionMessage.control = action[1] 
 
+    # publish message to topic
     TOC_ACTION_PUBLISHER.publish(actionMessage)
 
 
@@ -35,7 +41,7 @@ def main():
     rospy.loginfo("Info[toc_execution_node.main]: Instantiating the toc_execution node...")
     rospy.init_node('toc_execution_node', anonymous=True)
 
-    rospy.Subscriber('monitor/toc_mdp_state', String, callback)
+    rospy.Subscriber('monitor/toc_mdp_state', TocMdpState, toc_mdp_state_callback)
 
     rospy.loginfo("Info[toc_execution_node.main]: Spinning...")
     rospy.spin()
